9905be9f02ef497a1265b287cb5b5cc501fd4a63
cach007 <cach00789@gmail.com>
2022-11-18 04:34:20 +0900

    login, join 화면 완성

--- user/views.py
+++ user/views.py
@@ -5,3 +5,8 @@ from rest_framework.views import APIView
 class Join(APIView):
     def get(self, request):
         return render(request, "user/join.html")
+
+
+class Login(APIView):
+    def get(self, request):
+        return render(request, "user/login.html")
