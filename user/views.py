e7d8676994675d7cf79a0d6d09a1d6b9fabffd32
cach007 <cach00789@gmail.com>
2022-12-15 01:19:10 +0900

    Profile 구현

--- user/views.py
+++ user/views.py
@@ -1,3 +1,6 @@
+import os
+from uuid import uuid4
+
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -5,7 +8,7 @@ from rest_framework.views import APIView
 from django.contrib.auth.hashers import make_password
 
 from user.models import User
-
+from Unstagram.settings import MEDIA_ROOT
 
 class Join(APIView):
     def get(self, request):
@@ -21,7 +24,7 @@ class Join(APIView):
                             nickname=nickname,
                             name=name,
                             password=make_password(password),
-                            profile_image="default_profile.jpg")
+                            profile_image="default_profile.jfif")
 
         return Response(status=200)
 
@@ -52,4 +55,28 @@ class Logout(APIView):
     def get(self, request):
         request.session.flush()
         return render(request, "user/login.html")
+
+
+class UploadProfile(APIView):
+    def post(self, request):
+        # 일단 파일 불러와
+        file = request.FILES['file']
+        email = request.data.get('email')
+
+        uuid_name = uuid4().hex
+        save_path = os.path.join(MEDIA_ROOT, uuid_name)
+
+        with open(save_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+        profile_image = uuid_name
+        email = request.data.get('email')
+
+        user = User.objects.filter(email=email).first()
+        user.profile_image = profile_image
+        user.save()
+
+        return Response(status=200)
+
 
