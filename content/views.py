8fafdc36ce43eee255bd1b92902638fe6a0e1190
cach007 <cach00789@gmail.com>
2022-12-23 03:22:30 +0900

    Reply 구현

--- content/views.py
+++ content/views.py
@@ -7,15 +7,30 @@ from rest_framework.views import APIView
 
 from Unstagram.settings import MEDIA_ROOT
 from user.models import User
-from .models import Feed
+from .models import Feed, Reply, Like, Bookmark
 
 
 # Create your views here.
 class Main(APIView):
     def get(self, request):
         print("called get")
-        feed_list = Feed.objects.all().order_by('-id')
-
+        feed_object_list = Feed.objects.all().order_by('-id')
+        feed_list = []
+        for feed in feed_object_list:
+            user = User.objects.filter(email=feed.email).first()
+            reply_object_list = Reply.objects.filter(feed_id=feed.id)
+            reply_list = []
+            for reply in reply_object_list:
+                reply_list.append(dict(reply_content=reply.reply_content,
+                                       nickname=user.nickname))
+            feed_list.append(dict(id=feed.id,
+                                  image=feed.image,
+                                  content=feed.content,
+                                  like_count=feed.like_count,
+                                  profile_image=user.profile_image,
+                                  nickname=user.nickname,
+                                  reply_list=reply_list
+                                  ))
         email = request.session.get('email', None)
 
         if email is None:
@@ -42,10 +57,9 @@ class UploadFeed(APIView):
 
         image = uuid_name
         content = request.data.get('content')
-        user_id = request.data.get('user_id')
-        profile_image = request.data.get('profile_image')
+        email = request.session.get('email', None)
 
-        Feed.objects.create(image=image, content=content, user_id=user_id, profile_image=profile_image, like_count=0)
+        Feed.objects.create(image=image, content=content, email=email, like_count=0)
 
         return Response(status=200)
 
@@ -64,3 +78,14 @@ class Profile(APIView):
             return render(request, "user/login.html")
 
         return render(request, 'content/profile.html', context=dict(user=user))
+
+
+class UploadReply(APIView):
+    def post(self, request):
+        feed_id = request.data.get('feed_id', None)
+        reply_content = request.data.get('reply_content', None)
+        email = request.session.get('email', None)
+
+        Reply.objects.create(feed_id=feed_id, reply_content=reply_content, email=email)
+
+        return Response(status=200)
